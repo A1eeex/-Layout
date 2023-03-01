f8650ecd220d945f9213b0a0284d46f903a7ec9f
Alex <opetrovskyi423@gmail.com>
2023-03-01 19:16:14 +0100

    add modal

--- Yoga/js/costom.js
+++ Yoga/js/costom.js
@@ -68,3 +68,20 @@ $(".about_classes_info_all").slick({
         }
     }]
 })
+const modal = document.getElementById("modal");
+const btn = document.getElementById("open-modal-btn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
