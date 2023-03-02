ca2959958608e0ab7456e5b13bed8d9105c89806
Alex <opetrovskyi423@gmail.com>
2023-03-02 22:45:25 +0100

    send form

--- Yoga/js/costom.js
+++ Yoga/js/costom.js
@@ -72,16 +72,36 @@ const modal = document.getElementById("modal");
 const btn = document.getElementById("open-modal-btn");
 const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
+}
+
+function SendMail() {
+    const serviceID = 'service_2y2mail';
+    const templateID = 'template_xzzkf2h';
+    const params = {
+        form_name: document.getElementById('full_name').value,
+        email_form: document.getElementById('email_form').value,
+        select_option_form: document.getElementById('select_option_form').value,
+        message_form: document.getElementById('message_form').value
+    }
+    //service ID, template ID
+    emailjs.send(serviceID, templateID, params).then(function (res) {
+        document.getElementById('full_name').value=''
+        document.getElementById('email_form').value=''
+        document.getElementById('select_option_form').value=''
+        document.getElementById('message_form').value=''
+        console.log('success!' + res.status)
+    })
+        .catch((err) =>console.log(err));
 }
