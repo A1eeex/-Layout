19f82fbb7d7f4fb9837ac91a39d3719ee553d07c
Alex <opetrovskyi423@gmail.com>
2023-03-04 16:03:55 +0100

    add valid form

--- Yoga/js/costom.js
+++ Yoga/js/costom.js
@@ -85,6 +85,37 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+// validate form
+const full_name = document.querySelector('#full_name');
+const phone_number = document.querySelector('#phone_number');
+const emailInput = document.querySelector('#email_form');
+const submitButton = document.querySelector('#submit_form_btn');
+
+// Disable the submit button initially
+submitButton.disabled = true;
+
+// Add event listeners to each input field
+full_name.addEventListener('input', validateForm);
+phone_number.addEventListener('input', validateForm);
+emailInput.addEventListener('input', validateForm);
+
+// Function to validate the form
+function validateForm() {
+    if (full_name.value.trim() !== '' && phone_number.value.trim() !== '' && emailInput.value.trim() !== '' && isValidEmail(emailInput.value.trim())) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+}
+
+// Function to check if the email is valid
+function isValidEmail(email) {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+    return emailRegex.test(email);
+}
+
+
+// emailJs
 
 function SendMail() {
     const serviceID = 'service_2y2mail';
@@ -105,3 +136,4 @@ function SendMail() {
     })
         .catch((err) =>console.log(err));
 }
+
